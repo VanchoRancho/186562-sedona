062d51da638db32181942694a8b12b4a731f6285
Ivan Baranov <ibaranov86@gmail.com>
2016-09-05 00:42:43 +0300

    Финальная проверка

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-var link = document.querySelector(".booking-search-btn");
-var popup = document.querySelector(".booking-search-form");
-var send = document.querySelector(".booking-search-form-btn");
+var link = document.querySelector(".booking-btn");
+var popup = document.querySelector(".booking-form");
+var send = document.querySelector(".booking-form-search-btn");
 link.addEventListener("click", function(event) {
   event.preventDefault();
-  if (popup.classList.contains("booking-search-form-show")) {
-    popup.classList.remove("booking-search-form-show");
-    popup.classList.add("booking-search-form-hide");
+  if (popup.classList.contains("booking-form-show")) {
+    popup.classList.remove("booking-form-show");
+    popup.classList.add("booking-form-hide");
   } else {
-    popup.classList.remove("booking-search-form-hide");
-    popup.classList.remove("booking-search-form-send");
-    popup.classList.add("booking-search-form-show");
+    popup.classList.remove("booking-form-hide");
+    popup.classList.remove("booking-form-send");
+    popup.classList.add("booking-form-show");
   }
 });
 send.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.add("booking-search-form-send");
-  setTimeout(function(event) {popup.classList.remove("booking-search-form-show");
+  popup.classList.add("booking-form-send");
+  setTimeout(function(event) {popup.classList.remove("booking-form-show");
 }, 500);
 });
